e5b4030ab51ac2a303fbe8557faf3b7ce0c480bc
inbruk <35333872+inbruk@users.noreply.github.com>
2020-11-30 17:53:42 +0300

    Initial commit

---
+++ app/src/main/java/com/lessons/myapplication4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.lessons.myapplication4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Glide.with(this )
+                .load(R.drawable.ic_launcher_background)
+                .into( imageView);
+    }
+}
